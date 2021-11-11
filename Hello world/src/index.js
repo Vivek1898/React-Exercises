1a45ecc6dee26f008d5255f94e54f92f71b4785b
Vivek Singh <viveksingh27795@gmail.com>
2021-11-11 22:51:31 +0530

    hello world
    Signed-off-by: Vivek Singh <viveksingh27795@gmail.com>

--- Hello world/src/index.js
+++ Hello world/src/index.js
@@ -1,10 +1,8 @@
-import React from 'react'
-import  ReactDOM  from 'react-dom'
-
-//React element part of virtual dom
-const element=<h1>Hello World</h1>
-//1-what to render
-//2-where to render
-ReactDOM.render(element,document.getElementById('root'))
-//console.log(element);
-//for rendering in realDom imporetd react
+function test(){
+    for (var i=0; i<5;i++){
+        console.log(i);
+        //0 1 2 3 4
+    }
+   
+}
+test();
