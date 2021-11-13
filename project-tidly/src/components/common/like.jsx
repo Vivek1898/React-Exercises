f5157c14604f93f63d513c60a180eb1c39557e52
Vivek Singh <viveksingh27795@gmail.com>
2021-11-13 15:04:15 +0530

    State less functional component

--- project-tidly/src/components/common/like.jsx
+++ project-tidly/src/components/common/like.jsx
@@ -1,17 +1,27 @@
 import React, { Component } from 'react';
 //input lIkd :boolean
 //out -- Onclick
+// state less function
+const Like = props => {
+    let classes="fa fa-heart";
+    if(!props.liked) classes+="-o";
+    return ( 
+    <i onClick={props.onClick} style={{cursor:'pointer'}} className={classes} aria-hidden="true" ></i>
+    );
+}
+ 
 
-class Like extends React.Component {
+
+// class Like extends React.Component {
   
 
-    render() { 
-        let classes="fa fa-heart";
-        if(!this.props.liked) classes+="-o";
-        return ( 
-        <i onClick={this.props.onClick} style={{cursor:'pointer'}} className={classes} aria-hidden="true" ></i>
-        );
-    }
-}
+//     render() { 
+//         let classes="fa fa-heart";
+//         if(!this.props.liked) classes+="-o";
+//         return ( 
+//         <i onClick={this.props.onClick} style={{cursor:'pointer'}} className={classes} aria-hidden="true" ></i>
+//         );
+//     }
+// }
  
 export default Like;
