361aa847fad9793efcb16345d9022eacebaf8af4
Vivek Singh <viveksingh27795@gmail.com>
2021-11-17 01:53:06 +0530

    Adding routes nd Navbar

--- project-tidly/src/App.js
+++ project-tidly/src/App.js
@@ -1,13 +1,28 @@
 import React,{Component} from 'react';
+import { Route ,Redirect,Switch} from "react-router-dom";
 import Movies from './components/movies';
+import Customers from "./components/customers";
+import NotFound from "./components/notFound";
+import Rentals from "./components/rentals";
+import NavBar from './components/navBar';
 import './App.css';
 
 function App() {
   return (
-   <main className="container">
-   <Movies/>
+    <React.Fragment>
+  <NavBar/>
+  <main className="container">
+ <Switch>
+  <Route path="/movies" component={Movies}></Route>
+  <Route path="/customers" component={Customers}></Route>
+  <Route path="/rentals" component={Rentals}></Route>
+  <Route path="/not-found" component={NotFound}></Route>
+  <Redirect from="/" exact to="/movies"/>
+  <Redirect exact to="/not-found"/>
+  </Switch>
 
    </main>
+   </React.Fragment>
   );
 }
 
--- project-tidly/src/index.js
+++ project-tidly/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -9,11 +9,14 @@ import 'font-awesome/css/font-awesome.css'
 
 
 ReactDOM.render(
- 
-  <BrowserRouter>  <App /> </BrowserRouter>,
+  
+ <BrowserRouter><App/></BrowserRouter>,
   
   document.getElementById('root')
 );
+
+
+
 
 
 // If you want to start measuring performance in your app, pass a function
