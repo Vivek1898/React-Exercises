67e9d399098d59cb9f317502550fdf797cb4480f
Vivek Singh <viveksingh27795@gmail.com>
2021-11-16 18:46:30 +0530

    Query string

--- router-app/src/components/posts.jsx
+++ router-app/src/components/posts.jsx
@@ -1,6 +1,8 @@
 import React from "react";
-
-const Posts = ({match}) => {
+import queryString from 'query-string';
+const Posts = ({match,location}) => {
+// returns approved short object
+  queryString.parse(location.search);
   return (
     <div>
       <h1>Posts</h1>
