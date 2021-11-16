dd3e91836962a38499e446f8b4e17a93a2ac3af0
Vivek Singh <viveksingh27795@gmail.com>
2021-11-16 15:17:13 +0530

    Switch and exact

--- router-app/src/App.js
+++ router-app/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Route } from "react-router-dom";
+import { Route ,Switch} from "react-router-dom";
 import NavBar from "./components/navbar";
 import Products from "./components/products";
 import Posts from "./components/posts";
@@ -15,11 +15,14 @@ class App extends Component {
       <div>
         <NavBar />
         <div className="content">
-
+  {/* While using switcg other routes willl be ignored */}
+        <Switch>
 <Route path="/products" component={Products}/>
 <Route path="/posts" component={Posts}/>
 <Route path="/admin" component={Dashboard}/>
-<Route path="/" component={Home}/>
+<Route path="/"  component={Home}/>
+{/* Use exact for thath match exact path then render */}
+</Switch>
         </div>
 
       </div>
