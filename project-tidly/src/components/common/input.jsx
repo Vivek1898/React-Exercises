c9f310f3fe55c6e9167738c2962196e64084528c
Vivek Singh <viveksingh27795@gmail.com>
2021-12-13 13:46:45 +0530

    Basic Validation

--- project-tidly/src/components/common/input.jsx
+++ project-tidly/src/components/common/input.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 
-const Input = ({name,label,value,onChange}) => {
+const Input = ({name,label,value,error,onChange}) => {
     return ( 
         <div className="form-group">
         <label htmlFor={name}>{label}</label>
-        <input autoFocus 
+        <input 
         value={value}
         onChange={onChange}
         name={name} 
         id={name} type="text" className="form-control" />
+
+        {error && <div className="alert alert-danger">{error}</div>}
         </div>
 
      );
--- project-tidly/src/components/loginform.jsx
+++ project-tidly/src/components/loginform.jsx
@@ -4,20 +4,41 @@ import Input from './common/input';
 class LoginForm extends React.Component {
 // username=React.createRef();
 // password=React.createRef();
-
 // componentDidMount(){
 //     this.username.current.focus();
 // }
 state={
-    account:{username:'',password:''}
+    account:{username:'',password:''},
+    errors:{
+        username:'Username is Required'
+    }
 }
+//Using array for errors easy to find using find method
+//thahts why we use object
+validate=()=>{
+    const errors={};
+    const{account}=this.state;
+    if(account.username.trim()==='')
+     errors.username='Username is required ';
 
-    //handling form submisson bc it reloads whole page on refresh
+  if(account.password.trim()==='')
+  errors.password='Password is required ';
+  return Object.keys(errors).length===0 ? null : errors;
+   };
+    
+//handling form submisson bc it reloads whole page on refresh
     handleSubmit=e =>{
    e.preventDefault();
+   //render error messages
+const errors=this.validate();
+
+this.setState({errors : errors || {}  });
+if(errors) return;
 
 
     };
+
+
 //also e.currentTarget 
 handleChange=({currentTarget:input})=>{
     //clone
@@ -27,19 +48,28 @@ handleChange=({currentTarget:input})=>{
 }
 
     render() { 
-        const{account}=this.state;
+        const{account,errors}=this.state;
         return (
         <div>
             <h1>Login</h1>
             <form  onSubmit={this.handleSubmit}>
-               <Input name="username" value={account.username}
-               label="Username" onChange={this.handleChange} />
+               <Input name="username"
+                value={account.username}
+               label="Username" 
+               onChange={this.handleChange}
+               error={errors.username}
+                />
                 <br/>
-                <Input name="password" value={account.password}
-               label="Password" onChange={this.handleChange} />
+                <Input name="password" 
+                value={account.password}
+               label="Password" 
+               onChange={this.handleChange} 
+               error={errors.password}
+               />
 
                     <br/>
-                    <button className="btn btn-primary">Submit</button>
+                    <button 
+                    className="btn btn-primary">Submit</button>
             </form>
         </div>
         );
